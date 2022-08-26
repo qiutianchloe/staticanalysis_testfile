2620d8152c046c929c7b245dd80564442c5bb274
qiutianchloe <qiutianchloe@hotmail.com>
2022-08-26 12:32:29 +1000

    modified words_casts that runnable with Infer and record the results

--- underflow_examples/words_casts/README.md
+++ underflow_examples/words_casts/README.md
@@ -1,12 +1,12 @@
 ## Some Updates
 
-1.add #include "underflow.h" to include the head file
+2.change the name three test functions
 
-**how to fix the test function that defined three times
+1.add #include "underflow.h" to include the head file
 
 ## Results
 
 |Tool|Command Line|exec time|issues found                         |
 |----------------|-------------------------------|-----------------------------|---------------------------|
-|Underflow|`./Underflow.sh ./examples/arrays.c`|???s|???|
-|Pulse-x|| ms|Not work|
+|Underflow|`./Underflow.sh ./examples/words_casts`|???s|???|
+|Pulse-x|`infer --pulse-isl --pulse-only -- gcc -c -I./include/ ./examples/words_casts.c`|34.757ms|No issues found|
---
+++ underflow_examples/words_casts/words_casts.c
@@ -0,0 +1,68 @@
+#include "underflow.h"
+
+void testa(unsigned int x){
+  if (++x >= 0){
+  }
+}
+
+void testb(unsigned char x){
+  if (x >= 0){
+  }
+}
+
+void testc(unsigned char x){
+  if (x >= 256){
+  }
+}
+
+char retbig(){
+  return 257;
+}
+
+void blah(){
+  int x = retbig();
+  _(assert x == 1)
+}
+
+char callmewithchar(char c){
+  return (c+1);
+}
+
+int callmewithint(int i){
+  return (i+1);
+}
+
+
+void testcalls(int i, char c){
+  int i_copy = i;
+  int c_copy = c;
+  i = callmewithchar(i);
+  c = callmewithint(c);
+  _(assert i == (int)(((char)i_copy)+1))
+  _(assert c == (char)(((int)c_copy)+1))
+}
+
+void testbig(){
+  int x = 0xFFFFFFFF;
+  _(assert x == -1)
+}
+
+void testlong(){
+  long x = -1;
+  int xlow = x;
+  int xhigh = (x >> 32);
+  _(assert xlow == 0xFFFFFFFF)
+  _(assert xhigh == 0xFFFFFFFF)
+  unsigned long y = 0xEFFFFFFFFFFFFFFFUL;
+  unsigned int ylow = y;
+  unsigned int yhigh = (y >> 32);
+  _(assert ylow == 0xFFFFFFFF)
+ _(assert yhigh == 0xEFFFFFFF)
+}
+
+void testU(){
+  unsigned int x = 55U;
+  _(assert x == 55)
+}
+
+
