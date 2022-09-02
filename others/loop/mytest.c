5a1f73734e161faf9b457eb77a7f269269cde4f9
qiutianchloe <qiutianchloe@hotmail.com>
2022-09-03 01:01:55 +1000

    add if statement in the loop

--- others/loop/mytest.c
+++ others/loop/mytest.c
@@ -10,3 +10,22 @@ int main(){
     int y = *x; 
     return 0; 
 }
+
+/*
+#define NULL ((void*)0)
+
+int main(){   
+    int i;
+    int j=0;  
+    for(i=0; i<4; i++){
+        if(j<=0){
+            j++;  
+        }else{
+            int *x = NULL;  // x is a null pointer
+            int y = *x;
+            j--; 
+        }
+    }
+    return 0; 
+}
+*/
